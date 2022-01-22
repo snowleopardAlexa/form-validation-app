91bd51348734b2ad42349b3d9dfad07e290a7ea3
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-22 17:16:56 -0500

    destructure form inputs

--- src/App.js
+++ src/App.js
@@ -4,26 +4,78 @@ import FormInput from './components/FormInput';
 
 function App() {
 
-const [username, setUsername] = useState("")
-const [username, setUsername] = useState("")
-const [username, setUsername] = useState("")
-const [username, setUsername] = useState("")
+const [value, seValue] = useState({
+  username: "",
+  email: "",
+  firstName: "",
+  lastName: "",
+  birthday: "",
+  password: "",
+  confirmPassowrd: "",
+});
 
+const inputs = [
+  {
+    id: 1,
+    name: "username",
+    type: "text",
+    placeholder: "Username",
+    label: "Username"
+  },
+  {
+    id: 2,
+    name: "email",
+    type: "text",
+    placeholder: "Email",
+    label: "Email"
+  },
+  {
+    id: 3,
+    name: "firstName",
+    type: "text",
+    placeholder: "First Name",
+    label: "First Name"
+  },
+  {
+    id: 4,
+    name: "lastName",
+    type: "text",
+    placeholder: "Last Name",
+    label: "Last Name"
+  },
+  {
+    id: 5,
+    name: "birthday",
+    type: "text",
+    placeholder: "Birthday",
+    label: "Birthday"
+  },
+  {
+    id: 6,
+    name: "password",
+    type: "text",
+    placeholder: "Password",
+    label: "Password"
+  },
+  {
+    id: 7,
+    name: "confirmPassword",
+    type: "text",
+    placeholder: "Confirm Password",
+    label: "Confirm Password"
+  }
+]
 
-// use useRef instead of useState
+// prevent browser from refreshing
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-
   };
 
   return (
     <div className="app">
        <form onSubmit={handleSubmit}>
          <FormInput name="username" placeholder="Username" />
-         <FormInput name="email" placeholder="Email"/>
-         <FormInput name="fullname" placeholder="Full Name" />
-         <FormInput name="something" placeholder="something else" />
+         
          <button>Submit</button>
        </form>
     </div>
