616fabc50738b4596c11c842174a7bdf0eefa355
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-22 16:41:03 -0500

    create forminput

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import FormInput from './components/FormInput';
 
 function App() {
   return (
     <div className="App">
-     helo
+       <form>
+         <FormInput placeholder="Username" />
+         <FormInput placeholder="Email"/>
+         <FormInput placeholder="Full Name" />
+         <FormInput placeholder="else" />
+       </form>
     </div>
   );
 }
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,17 @@
+import './formnInput.css';
+
+const FormInput = () => {
+  return (
+    <div className="formInput">
+        <label>Username</label>
+        <input />
+    </div>
+  )
+};
+
+export default FormInput;
+
+
+
+
+
